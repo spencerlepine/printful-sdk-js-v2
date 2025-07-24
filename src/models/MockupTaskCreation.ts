be121aba75f8ad195ca3d88514418158a422d6a0
Spencer Lepine <60903378+spencerlepine@users.noreply.github.com>
2025-07-24 14:07:09 -0700

    release(v2.1.2-beta): update the OpenAPI spec.json
    > - source: blob:https://developers.printful.com/11c34042-9e1f-4afc-8f97-06c2a7b5968c
    > - docs: https://developers.printful.com/docs/v2-beta/

--- src/models/MockupTaskCreation.ts
+++ src/models/MockupTaskCreation.ts
@@ -9,6 +9,12 @@ export type MockupTaskCreation = {
      * Generated file format. PNG will have a transparent background, JPG will have a smaller file size.
      */
     format?: MockupTaskCreation.format;
+    /**
+     * Width of the mockup image in pixels. If not specified, the default value will be used. The default value is 1000px.
+     * If the value is specified as 2000 that means that resulting mockups will be 2000x2000px.
+     *
+     */
+    mockup_width_px?: number;
     products: Array<(CatalogMockupProduct | TemplateMockupProduct)>;
 };
 export namespace MockupTaskCreation {
